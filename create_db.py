6e1c4078e598bd6c950b250f9eed24d9a1e425a3
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-10-08 23:00:15 +0300

    Formatted create_db

--- create_db.py
+++ create_db.py
@@ -5,33 +5,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config.from_object("config.Config")
 db.init_app(app)
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
 
 
 class Message(db.Model):
-    __tablename__ = 'messages'
+    __tablename__ = "messages"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     content = db.Column(db.String(400), nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now())
 
 
 class Like(db.Model):
-    __tablename__ = 'likes'
+    __tablename__ = "likes"
     id = db.Column(db.Integer, primary_key=True)
-    message_id = db.Column(db.Integer, db.ForeignKey('messages.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    message_id = db.Column(db.Integer, db.ForeignKey("messages.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
 
-
- 
 with app.app_context():
     db.drop_all()
     time.sleep(3)
