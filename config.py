41c335102abcdf3578bda4c899ea5c39d12a918a
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2024-10-08 23:03:15 +0300

    Formatted config

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://softarch:123456@localhost/twitter2')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://softarch:123456@localhost/twitter2")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
